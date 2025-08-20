e92f5445b13d03c3fb9d34d329d741ec199304a8
Kolia Palahniuk <nikolas.palahniuk@zealicsolutions.com>
2025-08-20 15:01:40 +0200

    fix: oppen HTTP Get objects access to public bucket over HTTP

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -60,6 +60,26 @@ resource "aws_s3_bucket_cors_configuration" "public" {
   }
 }
 
+# Public bucket policy to allow public read access
+resource "aws_s3_bucket_policy" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.public.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.public]
+}
+
 # Private S3 Bucket
 resource "aws_s3_bucket" "private" {
   bucket        = "${var.project_name}-${var.environment}-private-${random_string.bucket_suffix.result}"
